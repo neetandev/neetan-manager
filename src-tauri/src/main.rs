13f6c3ecab7c4d578a1d24d261fb2b00d3d532f7
Nils Hasenbanck <nils@hasenbanck.de>
2026-05-05 17:42:26 +0200

    fix: Error 71 (Protocol error) on NVIDIA and Wayland

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // Workaround for: https://github.com/tauri-apps/tauri/issues/10702
+    unsafe {
+        std::env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
+    }
+
     neetan_manager_lib::run()
 }
